402df9fb8eea034e028803253e53bffc33e74eab
Damian Pierzchalski <naod3sign@gmail.com>
2018-03-23 22:31:37 +0100

    task(da-schema): added partialy function createJsObject(only primitive) && improved extendFunction

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const daSchema = require('./src/da-schema');
 
 module.exports.default = {
     checkJsObject: daSchema.checkJsObject,
-    checkSchema: daSchema.checkSchema
+    checkSchema: daSchema.checkSchema,
+    createJsObject: daSchema.createJsObject
 }
 
 module.exports.checkJsObject = daSchema.checkJsObject;
 module.exports.checkSchema = daSchema.checkSchema;
+module.exports.createJsObject = daSchema.createJsObject;
