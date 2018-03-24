3462d7a027090410a05f14375d53faa639f7de90
Damian Pierzchalski <naod3sign@gmail.com>
2018-03-24 12:15:03 +0100

    task(da-schema): fixed createJsObject for object type

--- src/da-schema.js
+++ src/da-schema.js
@@ -1,196 +1,206 @@
-const extendFunction = function(...args) {
-	let listOfFuncs = [];
-	for (let func of arguments) {
-		listOfFuncs.push(func);
-	}
-	return function() {
-		let result;
-		for (let func of listOfFuncs) {
-			if (!result) result = func(...arguments);
+;
+(() => {
+	const extendFunction = function(...args) {
+		let listOfFuncs = [];
+		for (let func of arguments) {
+			listOfFuncs.push(func);
 		}
-		return result;
+		return function() {
+			let result;
+			for (let func of listOfFuncs) {
+				if (!result) result = func(...arguments);
+			}
+			return result;
+		};
 	};
-};
 
-function createJsObject(schema, jsObject = {}) {
+	function createJsObject(schema, jsObject = {}, validSchema = true) {
+		if (validSchema) checkSchema(schema);
 
-	checkSchema(schema);
+		function generateValue() {
 
-	function generateValue() {
+			const base = function(jsObj, schema, key) {
+				if (schema.defaultValue) {
+					return {
+					[key]: schema.defaultValue
+					};
+				}
 
-		const base = function(schema, key) {
-			if (schema.optional) {
-				return {};
-			}
-			if (schema.defaultValue) {
+			};
+			const string = extendFunction(base, function(jsObj, schema, key) {
 				return {
-					[key]: schema.defaultValue
-				};
-			}
-
-		};
-		const string = extendFunction(base, function(schema, key) {
-			return {
 				[key]: 'stringValue'
-			};
-		});
+				};
+			});
 
-		const number = extendFunction(base, function(schema, key) {
-			return {
+			const number = extendFunction(base, function(jsObj, schema, key) {
+				return {
 				[key]: 1
-			};
-		});
+				};
+			});
 
-		const boolean = extendFunction(base, function(schema, key) {
-			return {
+			const boolean = extendFunction(base, function(jsObj, schema, key) {
+				return {
 				[key]: true
+				};
+			});
+
+			const object = extendFunction(base, function(jsObj, schema, key) {
+				let newObject = {};
+				if(schema.properties){
+				    newObject[key] = createJsObject(schema.properties, {},false);
+				} else{
+					newObject[key] = {};
+				}
+				return newObject;
+			});
+
+			return {
+				string,
+				number,
+				boolean,
+				object
 			};
-		});
+		}
 
-		return {
-			string,
-			number,
-			boolean
-		};
+		for (let key in schema) {
+			let type = schema[key].type;
+			let generateValueInstance = generateValue();
+
+			Object.assign(jsObject, generateValueInstance[type](jsObject, schema[key], key));
+		}
+
+		return jsObject;
 	}
 
-	for (let key in schema) {
-		let type = schema[key].type;
-		let generateValueInstance = generateValue();
-		Object.assign(jsObject, generateValueInstance[type](schema[key], key));
+	function checkJsObject(jsObject, schema, validSchema = true) {
+		if (validSchema) checkSchema(schema);
 
-	}
-
-	return jsObject;
-}
-
-function checkJsObject(jsObject, schema, validSchema = true) {
-	if (validSchema) checkSchema(schema);
-
-	function validJsObjectEngine() {
-		const string = function(objProp, schema, key) {
-			if (typeof objProp !== schema.type) {
-				throw `Incompatibility value ${objProp} typed as ${schema.type}`;
-			}
-		};
-		const boolean = function(objProp, schema, key) {
-			if (typeof objProp !== schema.type) {
-				throw `Incompatibility value ${objProp} typed as ${schema.type}`;
-			}
-		};
-		const number = function(objProp, schema, key) {
-			if (typeof objProp !== schema.type) {
-				throw `Incompatibility value ${objProp} typed as ${schema.type}`;
-			}
-		};
-		const object = function(objProp, schema, key) {
-			if (typeof objProp !== schema.type) {
-				throw `Incompatibility value ${objProp} typed as ${schema.type}`;
-			}
-			if (schema.properties) {
-				checkJsObject(objProp, schema.properties);
-			}
-		};
-		const array = function(objProp, schema, key) {
-			if (!Array.isArray(objProp)) {
-				throw `Incompatibility value ${objProp} typed as ${schema.type}`;
-			}
-			if (schema.items) {
-				objProp.forEach((prop, index, array) => {
-					function returnIndex(index, schemaItemLength) {
-						let finalIndex;
-						if (index >= schemaItemLength) {
-							finalIndex = index - schemaItemLength;
-						} else {
-							finalIndex = index;
+		function validJsObjectEngine() {
+			const string = function(objProp, schema, key) {
+				if (typeof objProp !== schema.type) {
+					throw `Incompatibility value ${objProp} typed as ${schema.type}`;
+				}
+			};
+			const boolean = function(objProp, schema, key) {
+				if (typeof objProp !== schema.type) {
+					throw `Incompatibility value ${objProp} typed as ${schema.type}`;
+				}
+			};
+			const number = function(objProp, schema, key) {
+				if (typeof objProp !== schema.type) {
+					throw `Incompatibility value ${objProp} typed as ${schema.type}`;
+				}
+			};
+			const object = function(objProp, schema, key) {
+				if (typeof objProp !== schema.type) {
+					throw `Incompatibility value ${objProp} typed as ${schema.type}`;
+				}
+				if (schema.properties) {
+					checkJsObject(objProp, schema.properties);
+				}
+			};
+			const array = function(objProp, schema, key) {
+				if (!Array.isArray(objProp)) {
+					throw `Incompatibility value ${objProp} typed as ${schema.type}`;
+				}
+				if (schema.items) {
+					objProp.forEach((prop, index, array) => {
+						function returnIndex(index, schemaItemLength) {
+							let finalIndex;
+							if (index >= schemaItemLength) {
+								finalIndex = index - schemaItemLength;
+							} else {
+								finalIndex = index;
+							}
+							return finalIndex;
 						}
-						return finalIndex;
+						checkJsObject({ arraySubProp: prop }, { arraySubProp: schema.items[returnIndex(index, schema.items.length)] });
+					});
+				}
+			};
+			return {
+				string,
+				array,
+				object,
+				boolean,
+				number
+			};
+		}
+
+		for (let key in schema) {
+			let type = schema[key].type;
+			let validJsObjectEngineInstance = validJsObjectEngine();
+			if (!jsObject.hasOwnProperty(key)) {
+				throw `Can't find ${key} in object ${JSON.stringify(jsObject)}`;
+			}
+			validJsObjectEngineInstance[type](jsObject[key], schema[key], key);
+		}
+
+	}
+
+	function checkSchema(schema) {
+		function validSchemaEngine() {
+			const base = function(schema, key) {
+				if (schema.hasOwnProperty('optional')) {
+					if (typeof schema.optional !== 'boolean') {
+						throw `Incompatibility value of optional option`;
 					}
-					checkJsObject({ arraySubProp: prop }, { arraySubProp: schema.items[returnIndex(index, schema.items.length)] });
-				});
-			}
-		};
-		return {
-			string,
-			array,
-			object,
-			boolean,
-			number
-		};
-	}
-
-	for (let key in schema) {
-		let type = schema[key].type;
-		let validJsObjectEngineInstance = validJsObjectEngine();
-		if (!jsObject.hasOwnProperty(key)) {
-			throw `Can't find ${key} in object ${JSON.stringify(jsObject)}`;
-		}
-		validJsObjectEngineInstance[type](jsObject[key], schema[key], key);
-	}
-
-}
-
-function checkSchema(schema) {
-	function validSchemaEngine() {
-		const base = function(schema, key) {
-			if (schema.hasOwnProperty('optional')) {
-				if (typeof schema.optional !== 'boolean') {
-					throw `Incompatibility value of optional option`;
 				}
-			}
-			if (schema.hasOwnProperty('defaultValue')) {
-				checkJsObject({
+				if (schema.hasOwnProperty('defaultValue')) {
+					checkJsObject({
 					[key]: schema.defaultValue
-				}, {
+					}, {
 					[key]: schema
-				}, false);
-			}
-		};
-		const string = extendFunction(base, function(schema, key) {});
-		const boolean = extendFunction(base, function(schema, key) {});
-		const number = extendFunction(base, function(schema, key) {});
-		const object = extendFunction(base, function(schema, key) {
-			if (schema.hasOwnProperty('properties')) {
-				let objProp = schema.properties;
-				for (let key in objProp) {
-					checkSchema({
+					}, false);
+				}
+			};
+			const string = extendFunction(base, function(schema, key) {});
+			const boolean = extendFunction(base, function(schema, key) {});
+			const number = extendFunction(base, function(schema, key) {});
+			const object = extendFunction(base, function(schema, key) {
+				if (schema.hasOwnProperty('properties')) {
+					let objProp = schema.properties;
+					for (let key in objProp) {
+						checkSchema({
 						[key]: objProp[key]
+						});
+					}
+				}
+			});
+			const array = extendFunction(base, function(schema, key) {
+				if (schema.hasOwnProperty('items')) {
+					schema.items.forEach((prop, index, array) => {
+						checkSchema({
+						[key]: prop
+						});
 					});
 				}
-			}
-		});
-		const array = extendFunction(base, function(schema, key) {
-			if (schema.hasOwnProperty('items')) {
-				schema.items.forEach((prop, index, array) => {
-					checkSchema({
-						[key]: prop
-					});
-				});
-			}
-		});
-		return {
-			string,
-			array,
-			object,
-			boolean,
-			number
-		};
-	}
-
-	for (let key in schema) {
-		let type = schema[key].type;
-		let validSchemaEngineInstance = validSchemaEngine();
-		if (Object.keys(validSchemaEngineInstance)
-			.indexOf(type) === -1) {
-			throw `Type ${type} not implemented`;
+			});
+			return {
+				string,
+				array,
+				object,
+				boolean,
+				number
+			};
 		}
-		validSchemaEngineInstance[type](schema[key], key);
+
+		for (let key in schema) {
+			let type = schema[key].type;
+			let validSchemaEngineInstance = validSchemaEngine();
+			if (Object.keys(validSchemaEngineInstance)
+				.indexOf(type) === -1) {
+				throw `Type ${type} not implemented`;
+			}
+			validSchemaEngineInstance[type](schema[key], key);
+		}
+
 	}
 
-}
-
-module.exports = {
-	checkSchema,
-	checkJsObject,
-	createJsObject
-}
+	module.exports = {
+		checkSchema,
+		checkJsObject,
+		createJsObject
+	};
+})();
